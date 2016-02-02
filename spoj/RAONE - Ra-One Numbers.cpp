d2949b9cab8a57e98623231b0aa968452a502a2c
Your Name <your@example.com>
2016-02-02 00:48:18 -0500

    finished RAONE

--- spoj/RAONE - Ra-One Numbers.cpp
+++ spoj/RAONE - Ra-One Numbers.cpp
@@ -7,49 +7,68 @@ using namespace std;
 
 typedef long long ll;
 
-int memo[19][201];
-ll dp[19][201];
+int memo[19][301];
+ll dp[19][301];
+int len;
 
 ll F(int dig, int sum){
 	if(dig == 0)return sum == 1;
 	if(memo[dig][sum + offset] == 1)return dp[dig][sum + offset];
-	
+
 	memo[dig][sum + offset] = 1;
-	
+
 	ll ans = 0LL;
-	
+
 	for(int i = 0 ; i < 10 ; ++i){
-		
+        if(dig %2 == 0) ans += F(dig - 1, sum + i);
+        else ans += F(dig - 1, sum -i);
 	}
-	
+
 	return dp[dig][sum + offset] = ans;
 }
 
 ll solve(ll x){
-	ll ans = 0LL;
-	
+	ll ans = 0LL, sum = 0LL;
+
 	char cad[20];
-	
+
 	sprintf(cad, "%lld", x);
-	
-	int l = strlen(cad);
-	
-	int j = l;
-	
+
+	len = strlen(cad);
+
+	int j = len;
+
+	for(int i = 0 ; i < len ; ++i){
+        j--;
+
+        int d = cad[i] - '0';
+
+        for(int k = 0 ; k < d; ++k){
+            if((j+1) %2 == 0){
+                ans += F(j, sum + k);
+            }
+            else{
+                ans += F(j, sum - k);
+            }
+        }
+        if((j+1) % 2 == 0)sum += d;
+        else sum -= d;
+	}
+
 	return ans;
 }
 
 int main(){
 	int T;
-	
+
 	scanf("%d", &T);
-	
+
 	ll a, b;
-	
+
 	for(int tc = 1 ; tc <= T ; ++tc){
-		scanf("%lld %lld", a, b);
+		scanf("%lld %lld", &a, &b);
 		printf("%lld\n", solve(b+1) - solve(a));
 	}
-	
+
 	return 0;
 }
