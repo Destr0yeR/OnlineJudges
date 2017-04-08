641a2efa28bfe8801f69947adad729a89e4634b0
Javier Palomino <jfpp92@gmail.com>
2017-04-07 20:41:07 -0500

    fix

--- codefights/bots/asana/projects.cpp
+++ codefights/bots/asana/projects.cpp
@@ -8,4 +8,14 @@ std::string smartAssigning(std::vector<std::string> n, std::vector<bool> s, std:
         mini = min(mini, t[i]);
         mp[t[i]].push_back(i);
     }
+
+    int x = mp[mini][0];
+
+    for(int i = 0 ; i < mp[mini].size() ; ++i) {
+        if(p[x] > p[mp[mini][i]]) {
+            x = mp[mini][i];
+        }
+    }
+
+    return n[x];
 }
