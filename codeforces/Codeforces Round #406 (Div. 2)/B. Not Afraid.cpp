f5d666236a1ca086369d721933599053d3fc2a5b
Javier Palomino <jfpp92@gmail.com>
2018-11-16 10:23:33 -0500

    New problems

---
+++ codeforces/Codeforces Round #406 (Div. 2)/B. Not Afraid.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+#define si(n) scanf("%d", &n)
+#define sii(n, m) scanf("%d %d", &n, &m)
+#define sc(c) scanf("%c", &c)
+#define ss(s) scanf("%s", s)
+
+#define forn(i, n) for(int i = 0 ; i < n ; ++i)
+#define forr(i, a, b) for(int i = a ; i < b ; ++i)
+
+#define sz(x) (int)x.size()
+
+#define rforn(i, n) for(int i = n-1 ; i >= 0 ; --i)
+#define rforr(i, a, b) for(int i = b-1 ; i >= a ; --i)
+
+#define mset(x, y) memset(x, y, sizeof(x))
+#define all(x) x.begin(), x.end()
+
+#define TEST(t) int T; cin >> T; for(int t = 1 ; t <= T ; ++t)
+
+#ifdef ONLINE_JUDGE
+	#define sll(x) scanf("%I64d", &x)
+#else
+	#define sll(x) cin >> x
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef complex<long double> cd;
+
+typedef vector<int> vi;
+typedef vector<cd> vcd;
+
+const double PI = 4*atan(1);
+const double TPI = 2*PI;
+
+int main(){
+	int n, m; sii(n, m);
+	bool cancel = false;
+	forn(i, m) {
+		set<int> s;
+		
+		string line;
+		cin.ignore();
+		getline(cin, line);
+		
+		stringstream ss;
+		
+		ss << line;
+		
+		bool partial = true;
+		
+		int x;
+		while(ss >> x) {
+			if(s.find(x * -1) != s.end()) {
+				partial = false;
+			}
+			s.insert(x);
+		}
+		
+		cancel |= partial;
+	}
+	
+	if(cancel)puts("YES");
+	else puts("NO");
+	
+	return 0;
+}
